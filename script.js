ebe3d13546910e55578836d0c53b6fd786f67398
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-03-23 15:49:46 -0300

    if great

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-let sayYourCep = document.getElementById("cep_what").value;
-const options = {
-    mode: "no-cors",
-    Headers: "Content-Type: application/json",
-}
-function getCep() {
-  fetch(`https://viacep.com.br/ws/${sayYourCep}/json/`,options)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      validationAll();
-      city.textContent = data.localidade;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+function getCep(){
 
-function validationAll() {
-  validationField();
-  validationForm();
-}
-
-function validationField() {
-  if (sayYourCep.value === "") {
-    alert("Preencha os campos");
-  }
+    let sayYourCep = document.getElementById("cep_what").value;
+    if(sayYourCep === ""){
+        alert("Preencha os campos");
+        return;
+    }
+    fetch( `https://viacep.com.br/ws/${sayYourCep}/json/`)
+    .then((response) =>{
+        return response.json()
+    }).then((data)=>{
+      
+            city.textContent = data.bairro;
+    }).catch((err)=>{
+        console.log(err);
+    })
 }
