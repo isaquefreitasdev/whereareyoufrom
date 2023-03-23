63ef4884481ce17eb3a5104a014be8cae6894f6c
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-03-23 15:32:01 -0300

    With error

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-
-
-
-function getCep(){
-    let sayYourCep = document.getElementById("cep_what").value
-    fetch( `https://viacep.com.br/ws/${sayYourCep}/json/`,)
-    .then((response) =>{
-        return response.json()
-    }).then((data)=>{
-        console.log(data.bairro)
-    }).catch((err)=>{
-        console.log(err);
+let sayYourCep = document.getElementById("cep_what").value;
+const options = {
+    mode: "no-cors",
+    Headers: "Content-Type: application/json",
+}
+function getCep() {
+  fetch(`https://viacep.com.br/ws/${sayYourCep}/json/`,options)
+    .then((response) => {
+      return response.json();
     })
-   
+    .then((data) => {
+      validationAll();
+      city.textContent = data.localidade;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function validationAll() {
+  validationField();
+  validationForm();
+}
+
+function validationField() {
+  if (sayYourCep.value === "") {
+    alert("Preencha os campos");
+  }
 }
