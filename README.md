062dd6faff4ff70a9fc8503a13a0c4ed58d0c4a6
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-03-27 10:47:44 -0300

    Prototype/Final

---
+++ README.md
@@ -0,0 +1,20 @@
+<img align="center" src='.Untitled design.png'><br>
+<h1 align="center">Cep Search</h1><br>
+<p align="center">Descubra qual cidade,estado e bairro que você está!</p><br>
+
+<h2>Propósito</h2>
+<p>Desenvolver uma apliacação que posso mostrar onde o usuário está,que serve para ser usado em conjunto com buscadores de endereço geralmente usado em ecommerces de produtos</p>
+<br>
+<h3>Ferramentas</h3>
+<br>
+<ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+    <li>Visual Studio Code</li>
+    <li>Api Via Cep</li>
+</ul>
+
+
+<h4>Links</h4>
+<p><a href=""></a>
--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const sayCep = document.getElementById("sayCep")
+const sayCep = document.getElementById("sayCep");
+
 async function getCep(){
     
-    let sayYourCep = document.getElementById("field").value;
+    let sayYourCep = document.getElementById("field").value.replace("-","");
     const conditionCep = sayYourCep.lenght > 8
    if(sayYourCep !== ""  || !conditionCep){
     let res = await fetch( `https://viacep.com.br/ws/${sayYourCep}/json/`)
     let data = await res.json();
-    city.innerText = data.localidade;   
+    adressCompleted.innerText = `${data.localidade} - ${data.uf} - ${data.bairro}`;   
     document.getElementById("field").value = "";
     }else{
         alert("Preencha por favor")
     }
 
 }
-
 
 
