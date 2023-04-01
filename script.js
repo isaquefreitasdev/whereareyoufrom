829206dd4d4ff9a8a920bd61e1e7c5eff1ba93f7
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-03-31 23:11:08 -0300

    if

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 const sayCep = document.getElementById("sayCep");
 
-async function getCep(){
-    
-    let sayYourCep = document.getElementById("field").value.replace("-","");
-    const conditionCep = sayYourCep.lenght > 8
-   if(sayYourCep !== ""  || !conditionCep){
-    let res = await fetch( `https://viacep.com.br/ws/${sayYourCep}/json/`)
+async function getCep() {
+  let sayYourCep = document.getElementById("field").value.replace("-", "");
+  const conditionCep = sayYourCep.lenght > 8;
+  if (sayYourCep === "") {
+    alert("Preencha campos")
+  } else {
+    let res = await fetch(`https://viacep.com.br/ws/${sayYourCep}/json/`);
     let data = await res.json();
-    adressCompleted.innerText = `${data.localidade} - ${data.uf} - ${data.bairro}`;   
+    adressCompleted.innerText = `${data.localidade} - ${data.uf} - ${data.bairro}`;
     document.getElementById("field").value = "";
-    }else{
-        alert("Preencha por favor")
-    }
-
+  }
 }
-
-
