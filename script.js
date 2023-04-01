955556f0854f060bd9a13c4a43c65f2cff8cb62d
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-04-01 12:33:42 -0300

    final

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 const sayCep = document.getElementById("sayCep");
+const adressCompleted = document.getElementById("adressCompleted");
 
 async function getCep() {
+  
   let sayYourCep = document.getElementById("field").value.replace("-", "");
   const conditionCep = sayYourCep.lenght > 8;
+  
   if (sayYourCep === "") {
     alert("Preencha campos")
+    adressCompleted.classList.remove("display")
   } else {
     let res = await fetch(`https://viacep.com.br/ws/${sayYourCep}/json/`);
     let data = await res.json();
-    adressCompleted.innerText = `${data.localidade} - ${data.uf} - ${data.bairro}`;
+    if (data.erro) {
+      alert("CEP não encontrado");
+      adressCompleted.classList.remove("display")
+    } else if(data.bairro === "") {
+      adressCompleted.classList.add("display")
+      
+      adressCompleted.innerText = `${data.localidade} - ${data.uf}`;
+    }else{
+      adressCompleted.classList.add("display")
+      
+      adressCompleted.innerText = `${data.localidade} - ${data.uf} | ${data.bairro}`;
+
+
+    }
+
     document.getElementById("field").value = "";
   }
+  
 }
